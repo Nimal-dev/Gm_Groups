9e99b0a36cfd7c83f113da797dc9bcf472bc5e2a
Nimal-Dev <nimalprince.dev@gmail.com>
2025-08-07 13:04:52 +0530

    Xlantis Dc link added
    Added xlantis dc link to the footer

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,5 +5,6 @@ export function Logo() {
     <Link href="/" className="text-2xl font-bold tracking-tighter text-white uppercase transition-colors font-headline hover:text-accent">
       GM Groups <span className="text-accent">Pvt Ltd</span>
     </Link>
+
   );
 }
